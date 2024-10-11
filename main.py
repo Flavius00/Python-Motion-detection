290cbfdbae845e303bfe9f2860de5b56b4708218
flavius <flaviusturcu11@gmail.com>
2024-10-11 12:46:06 +0300

    o versiune functionala

--- main.py
+++ main.py
@@ -1,60 +1,107 @@
-import cv2
-import time
-import datetime
-import imutils
+# Python program to implement
+# Webcam Motion Detector
 
-def motion_detection():
-    video_capture = cv2.VideoCapture(0)
-    time.sleep(2)
+# importing OpenCV, time and Pandas library
+import cv2, time, pandas
+# importing datetime class from datetime library
+from datetime import datetime
 
-    first_frame = None
+# Assigning our static_back to None
+static_back = None
 
-    while True:
-        ret, frame = video_capture.read()
-        if not ret:
-            break
+# List when any moving object appear
+motion_list = [ None, None ]
 
-        text = 'Unoccupied'
+# Time of movement
+time = []
 
-        greyscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gaussian_frame = cv2.GaussianBlur(greyscale_frame, (21, 21), 0)
+# Initializing DataFrame, one column is start
+# time and other column is end time
+df = pandas.DataFrame(columns = ["Start", "End"])
 
-        greyscale_image = gaussian_frame
+# Capturing video
+video = cv2.VideoCapture(0)
 
-        if first_frame is None:
-            first_frame = greyscale_image
-            continue
+# Infinite while loop to treat stack of image as video
+while True:
+	# Reading frame(image) from video
+	check, frame = video.read()
 
-        frame = imutils.resize(frame, width=600)
-        frame_delta = cv2.absdiff(first_frame, greyscale_image)
+	# Initializing motion = 0(no motion)
+	motion = 0
 
-        thresh = cv2.threshold(frame_delta, 50, 225, cv2.THRESH_BINARY)[1]
-        dilate_image = cv2.dilate(thresh, None, iterations=2)
+	# Converting color image to gray_scale image
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        cnt = cv2.findContours(dilate_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnt = cnt[0] if len(cnt) == 2 else cnt[1]
+	# Converting gray scale image to GaussianBlur
+	# so that change can be find easily
+	gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-        for c in cnt:
-            if cv2.contourArea(c) > 800:
-                (x, y, w, h) = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                text = 'Occupied'
+	# In first iteration we assign the value
+	# of static_back to our first frame
+	if static_back is None:
+		static_back = gray
+		continue
 
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(frame, f'[+] Room Status: {text}', (10, 20), font, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, datetime.datetime.now().strftime('%A %d %B %Y %I:%M:%S%p'),
-                    (10, frame.shape[0] - 10), font, 0.35, (0, 0, 255), 1)
+	# Difference between static background
+	# and current frame(which is GaussianBlur)
+	diff_frame = cv2.absdiff(static_back, gray)
 
-        cv2.imshow('Video', frame)
-        cv2.imshow('Foreground Mask', dilate_image)
-        cv2.imshow('Frame Delta', frame_delta)
+	# If change in between static background and
+	# current frame is greater than 30 it will show white color(255)
+	thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
+	thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
 
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
-            break
+	# Finding contour of moving object
+	cnts,_ = cv2.findContours(thresh_frame.copy(),
+					cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    video_capture.release()
-    cv2.destroyAllWindows()
+	for contour in cnts:
+		if cv2.contourArea(contour) < 10000:
+			continue
+		motion = 1
 
-if __name__ == '__main__':
-    motion_detection()
+		(x, y, w, h) = cv2.boundingRect(contour)
+		# making green rectangle around the moving object
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+
+	# Appending status of motion
+	motion_list.append(motion)
+
+	motion_list = motion_list[-2:]
+
+	# Appending Start time of motion
+	if motion_list[-1] == 1 and motion_list[-2] == 0:
+		time.append(datetime.now())
+
+	# Appending End time of motion
+	if motion_list[-1] == 0 and motion_list[-2] == 1:
+		time.append(datetime.now())
+
+	# Displaying image in gray_scale
+	cv2.imshow("Gray Frame", gray)
+
+	# Displaying the difference in currentframe to
+	# the staticframe(very first_frame)
+	cv2.imshow("Difference Frame", diff_frame)
+
+	# Displaying the black and white image in which if
+	# intensity difference greater than 30 it will appear white
+	cv2.imshow("Threshold Frame", thresh_frame)
+
+	# Displaying color frame with contour of motion of object
+	cv2.imshow("Color Frame", frame)
+
+	key = cv2.waitKey(1)
+	# if q entered whole process will stop
+	if key == ord('q'):
+		# if something is movingthen it append the end time of movement
+		if motion == 1:
+			time.append(datetime.now())
+		break
+
+
+video.release()
+
+# Destroying all the windows
+cv2.destroyAllWindows()
